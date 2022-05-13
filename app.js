b222674e0ec0559032cfa1e5675d7f677214aace
JulnCaraveo <alix_caraveo@hotmail.com>
2022-05-12 20:27:14 -0500

    mi primer server

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const port =3000//puerto por default
 app.get('/',(req, res) =>{
     res.send("Hola a todos")
 }) 
+//url diferente localhost:3000/launchx
+app.get('/launchx',(req, res) =>{
+    res.send("Hola a launchX")
+}) 
+//mandando objetos 
+app.get('/node',(req, res) =>{
+    const explorer={name:"Julian",apellido:"caraveo"}
+    res.send([explorer.name,explorer.apellido])
+
+}) 
 
 app.listen(port,()=>{
     console.log("server listo")
